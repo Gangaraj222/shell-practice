ed211751712161e0610ebb174032e13d397a48cb
Gangaraju <ug.raju11@gmail.com>
2026-03-28 00:55:00 +0530

    conversation

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
+#!/bin/bash 
 
-PERSON1=Modi #there should be no space before and after =
-PERSON2=Musk
+PERSON1=Trump
+PERSON2=Modi
 
-echo "$PERSON1:: Hey $PERSON2, How are you?"
-echo "$PERSON2:: Hello $PERSON1, I am fine. How are you doing"
-echo "$PERSON1:: I am fine too. What's up?"
-echo "$PERSON2:: Nohting, just going to Mars now, will you come?"
-echo "$PERSON1:: Sorry, you carry on! I will come once you come back"
+echo "$PERSON1:: Hey $PERSON2 how are you doing"
+echo "$PERSON2:: I am good how are you"
