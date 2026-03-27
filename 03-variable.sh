bb06144362bc9b2d38908043f3dc10b188ebb725
Gangaraju <ug.raju11@gmail.com>
2026-03-28 00:48:07 +0530

    conversation

--- 03-variable.sh
+++ 03-variable.sh
@@ -3,5 +3,5 @@
 PERSON1=Trump
 PERSON2=Modi
 
-echo "$PERSON1: Hey $PERSON2 how are you doing"
-echo "$PERSON2: I am good how are you"
+echo "$PERSON1:: Hey $PERSON2 how are you doing"
+echo "$PERSON2:: I am good how are you"
