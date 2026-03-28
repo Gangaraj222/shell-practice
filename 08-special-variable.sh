75c8039818dfbb4e734fa611e7b549ce983be84c
Gangaraju <ug.raju11@gmail.com>
2026-03-28 18:53:56 +0530

    conversation

--- 08-special-variable.sh
+++ 08-special-variable.sh
@@ -2,3 +2,7 @@
 
 echo " all variable passed the script: $@"
 echo " Number of variables passed: $#"
+echo "Script name: $0"
+echo "Current Directory: $PWD"
+echo "User running this script: $USER"
+echo "Home directory of user: $HOME"
