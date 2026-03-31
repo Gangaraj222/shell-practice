c787a1cc1ea9f037b76d728e49b75de860a615ac
Gangaraju <ug.raju11@gmail.com>
2026-03-31 18:09:23 +0530

    conversation

--- 14-logs.sh
+++ 14-logs.sh
@@ -12,3 +12,11 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
